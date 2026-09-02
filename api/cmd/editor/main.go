ac93437f02bcdc795fdbe65571d8ae5a7445c7e5
90186783 <90186783@bri.co.id>
2026-09-02 11:39:53 +0700

    fix: editor get website id from query param

--- api/cmd/editor/main.go
+++ api/cmd/editor/main.go
@@ -11,19 +11,28 @@ func main() {
 	port := renderer.EnvOr("EDITOR_PORT", "8080")
 	apiURL := renderer.EnvOr("API_URL", "http://api:8080")
 	timeoutMS := renderer.EnvOr("EDITOR_API_TIMEOUT_MS", "1500")
-	defaultID := renderer.EnvOr("EDITOR_WEBSITE_ID", "-")
 	staticDir := renderer.ResolveStaticDir()
 	cacheDir := renderer.ResolveCacheDir()
 
 	mux := http.NewServeMux()
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticDir))))
-	mux.HandleFunc("/", renderer.NewPageHandler(renderer.HandlerConfig{
-		APIURL:    apiURL,
-		CacheDir:  cacheDir,
-		DefaultID: defaultID,
-		Timeout:   renderer.ParseTimeout(timeoutMS),
-		Editable:  true,
-	}))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("website_id")
+
+		if id == "" {
+			id = r.URL.Query().Get("page_id")
+		}
+
+		handler := renderer.NewPageHandler(renderer.HandlerConfig{
+			APIURL:    apiURL,
+			CacheDir:  cacheDir,
+			DefaultID: id,
+			Timeout:   renderer.ParseTimeout(timeoutMS),
+			Editable:  true,
+		})
+
+		handler(w, r)
+	})
 
 	println("Editor server running at http://localhost:" + port)
 	println("Serving static assets from", filepath.Clean(staticDir))
